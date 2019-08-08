55410ae57a542b2bc706359d66d8d811c29c656b
Paweł Trojnacki <p.trojnacki@o2.pl>
2019-08-08 16:24:21 +0200

    Updates

---
+++ precache-manifest.ccabe5b05d75469a378ff11f4def63aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105bc2b6657c6dbdd38a069e87345ff3",
+    "url": "/spa-website/index.html"
+  },
+  {
+    "revision": "053d95f6530d78af358b",
+    "url": "/spa-website/static/css/main.ffdc7638.chunk.css"
+  },
+  {
+    "revision": "9339bafa23a17a61117c",
+    "url": "/spa-website/static/js/2.bc4fe0e8.chunk.js"
+  },
+  {
+    "revision": "053d95f6530d78af358b",
+    "url": "/spa-website/static/js/main.1d1a6234.chunk.js"
+  },
+  {
+    "revision": "945696302be79e00bf33",
+    "url": "/spa-website/static/js/runtime~main.1f0eb546.js"
+  },
+  {
+    "revision": "ada0965233c5ada8e47e708158551eab",
+    "url": "/spa-website/static/media/img1.ada09652.jpg"
+  },
+  {
+    "revision": "d1ba9782a9ee83d7002eb9a1422e7295",
+    "url": "/spa-website/static/media/img2.d1ba9782.jpg"
+  },
+  {
+    "revision": "f50203a1774c1f442c2d0d64e4f01cb8",
+    "url": "/spa-website/static/media/img3.f50203a1.jpg"
+  },
+  {
+    "revision": "c046d27142b18b3f3990d773d251ac7c",
+    "url": "/spa-website/static/media/img4.c046d271.jpg"
+  }
+]);
